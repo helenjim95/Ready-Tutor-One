24d3a3eaa6f87ee794ac7c25caf29f54a902f57c
ge32goy <hanyin.chen@tum.de>
2022-12-09 22:27:46 +0100

    commit

--- src/de/tum/in/ase/Artemis.java
+++ src/de/tum/in/ase/Artemis.java
@@ -43,13 +43,14 @@ public class Artemis{
 
     // TODO:
     public static String createSimpleReport(Stream<Exam> exam) {
-       Formatter formatter_ = new Formatter() {
-              @Override
-                public String formatExam(Exam exam) {
-                    return String.format("[%s] Exam \"%s\": %,.1f", exam.getGrade().getStatus().toString(), exam.getName(), exam.getGrade().getValue());
-                }
-       };
-       return exam.map(exam_ -> formatter_.formatExam(exam_)).collect(Collectors.joining(System.lineSeparator()));
-//        return createFormattedReport(exam, formatter -> formatter_.formatExam(exam));
+        return createFormattedReport(exam, exam1 -> String.format("[%s] Exam \"%s\": %,.1f", exam1.getGrade().getStatus().toString(), exam1.getName(), exam1.getGrade().getValue());
+//       return exam.map(exam_ -> {
+//           return createFormattedReport(exam, formatter -> new Formatter() {
+//               @Override
+//               public String formatExam(Exam exam) {
+//                   return String.format("[%s] Exam \"%s\": %,.1f", exam.getGrade().getStatus().toString(), exam.getName(), exam.getGrade().getValue());
+//               }
+//           }.toString());
+//       }).toString();
     }
 }
