09ada1a8d31f7e2e686bf9a96c7860a91004ffe3
ge32goy <hanyin.chen@tum.de>
2022-12-09 22:09:00 +0100

    commit

--- src/de/tum/in/ase/Artemis.java
+++ src/de/tum/in/ase/Artemis.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class Artemis {
+public class Artemis{
     // calculate the average grade of all valid exams
     public static double averageGrade(Stream<Exam> exams) {
         return exams.map(Exam::getGrade)
@@ -35,12 +35,6 @@ public class Artemis {
                 .collect(Collectors.groupingBy(Exam::isOnline, Collectors.summingInt(exam -> 1)));
     }
 
-//    // TODO: calculate the students performance index according to the exercise
-//    public static double calculatePerformanceIndex(Stream<Exam> exams) {
-//        return 0;
-//    }
-
-
 	// create a report using the passed formatter
 	public static String createFormattedReport(Stream<Exam> exams, Formatter formatter) {
 		return exams.map(exam -> formatter.formatExam(exam))
@@ -48,9 +42,13 @@ public class Artemis {
 	}
 
     // TODO:
-    public static String createSimpleReport(Stream<Exam> exams) {
-        return createFormattedReport(exams, formatter
-                -> exams.map(exam -> "[" + exam.getGrade().getStatus() + "] Exam \"" + exam.getName() + "\": " + exam.getGrade().getValue())
-                .collect(Collectors.joining(System.lineSeparator())));
+    public static String createSimpleReport(Stream<Exam> exam) {
+       Formatter formatter_ = new Formatter() {
+            @Override
+            public String formatExam(Exam exam) {
+                return String.format("[%s] Exam \"%s\": %d" + exam.getGrade().getStatus().toString(), exam.getName(), exam.getGrade().getValue());
+            }
+        };
+        return createFormattedReport(exam, formatter_);
     }
 }
